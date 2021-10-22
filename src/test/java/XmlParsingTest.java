eeba7e47d7ab873b72258ab4819b7ca1cb42dcc3
Dmitrii Pisarenko <d.pisarenko@jit.at>
2021-10-22 17:49:43 +0200

    Refactoring.

--- src/test/java/XmlParsingTest.java
+++ src/test/java/XmlParsingTest.java
@@ -7,11 +7,11 @@ import java.io.IOException;
 import java.io.StringReader;
 
 public class XmlParsingTest {
+    static final String XML = "<LetsGoBrandon></LetsGoBrandon><LetsGoBrandon></LetsGoBrandon>";
+
     @Test
     public void test() throws IOException, SAXException {
         final DOMParser parser = new DOMParser();
-        final String xml = "<LetsGoBrandon></LetsGoBrandon><LetsGoBrandon></LetsGoBrandon>";
-        parser.parse(new InputSource(new StringReader(xml)));
+        parser.parse(new InputSource(new StringReader(XML)));
     }
-
 }
