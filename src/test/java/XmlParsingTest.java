88243a31761c65febb4e079390a1c275eea7e51b
Dmitrii Pisarenko <d.pisarenko@jit.at>
2021-10-22 17:52:50 +0200

    Tried to use SAXParser instead of DOMParser.

--- src/test/java/XmlParsingTest.java
+++ src/test/java/XmlParsingTest.java
@@ -2,7 +2,11 @@ import com.sun.org.apache.xerces.internal.parsers.DOMParser;
 import org.junit.Test;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import java.io.IOException;
 import java.io.StringReader;
 
@@ -13,5 +17,12 @@ public class XmlParsingTest {
     public void test() throws IOException, SAXException {
         final DOMParser parser = new DOMParser();
         parser.parse(new InputSource(new StringReader(XML)));
+    }
+
+    @Test
+    public void testSax() throws IOException, SAXException, ParserConfigurationException {
+        final SAXParserFactory parserFactory = SAXParserFactory.newInstance();
+        final SAXParser parser = parserFactory.newSAXParser();
+        parser.parse(new InputSource(new StringReader(XML)), new DefaultHandler());
     }
 }
